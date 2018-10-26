aa158e4a42c42d7e6a4c0700c39c5717f324daaa
ardeleanraz <ardeleanrazvan111@yahoo.com>
2018-10-26 19:05:41 +0300

    update django-site

--- CompData/models.py
+++ CompData/models.py
@@ -2,24 +2,29 @@ from django.db import models
 
 
 class Employee(models.Model):
-    employee_name = models.TextField(default='')
+    employee = models.TextField(default='')
     telephone_number = models.CharField(max_length=10)
     departament_name = models.TextField(default='')
 
+    def __str__(self):
+        return 'Nume_angajat' + ':' + str(self.employee) + ',' + 'Id_angajat' + ':' + str(
+            self.id) + ',' + 'Numar_de_telefon' + ':' + str(
+            self.departament_name)
+
 
 class Departament(models.Model):
-    departament_name = models.TextField(default='')
-    manager_name = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True)
+    departament = models.TextField(default='')
+    manager = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True)
 
 
 class Product(models.Model):
-    product_name = models.TextField(default='')
+    product = models.TextField(default='')
     price = models.IntegerField(default=0)
     departament = models.ForeignKey(Departament, on_delete=models.SET_NULL, null=True)
 
 
 class Sale(models.Model):
-    product = models.ForeignKey(Product , on_delete=models.SET_NULL,null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     year = models.IntegerField(default=0)
     month = models.IntegerField(default=0)
     day = models.IntegerField(default=0)
